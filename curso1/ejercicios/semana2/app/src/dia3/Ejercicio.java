6c2686d354da04d6522801b9ada3d3efc8f0ff65
Fatine <fatinemamnoun@gmail.com>
2025-04-23 20:25:12 +0200

    Ejercicios 1 semana 2

---
+++ curso1/ejercicios/semana2/app/src/dia3/Ejercicio.java
@@ -0,0 +1,13 @@
+package dia3;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        int resultado = sumarNumeros(5, 3);
+        System.out.println(resultado);
+    }
+
+    private static int sumarNumeros(int num1, int num2) {
+        return num1 + num2;
+
+    }
+}
